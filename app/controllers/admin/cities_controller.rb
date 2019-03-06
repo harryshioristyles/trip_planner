3429fa615abfc494d7a38044eb646577c86b8493
harryshioristyles <imshiori46@gmail.com>
2019-03-06 17:06:51 +0900

    Add cocoon to country_model and cities_controller

--- app/controllers/admin/cities_controller.rb
+++ app/controllers/admin/cities_controller.rb
@@ -1,5 +1,7 @@
 class Admin::CitiesController < ApplicationController
   def index
+    @country = Country.new
+    @city = @country.cities.build
   end
 
   def create
@@ -13,4 +15,11 @@ class Admin::CitiesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def country_params
+        params.require(:country).permit(:country_name,:area, cities_attributes: [:id, :city_name, :country_id, :_destroy])
+    end
+
 end
--- app/models/country.rb
+++ app/models/country.rb
@@ -1,6 +1,7 @@
 class Country < ApplicationRecord
 
-	has_many :cities, dependent: :destroy
+	has_many :cities, inverse_of: :country, dependent: :destroy
+  accepts_nested_attributes_for :cities, reject_if: :all_blank, allow_destroy: true
 
   enum area: {asia: 0, middle_east: 1, europe: 2, africa: 3, north_america: 4, south_america: 5, oceania: 6}
 
