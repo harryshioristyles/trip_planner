b8d8a6e58188c6769bca13641a4c0e229d7b8377
harryshioristyles <imshiori46@gmail.com>
2019-03-06 16:13:14 +0900

    Add enum to country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,4 +2,6 @@ class Country < ApplicationRecord
 
 	has_many :cities, dependent: :destroy
 
+  enum area: {asia: 0, middle_east: 1, europe: 2, africa: 3, north_america: 4, south_america: 5, oceania: 6}
+
 end
