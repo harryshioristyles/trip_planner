e15bb7ca3bfecc58566db4abb8a975ec93dcc024
harryshioristyles <imshiori46@gmail.com>
2019-03-06 14:58:55 +0900

    Add country model and DB

---
+++ db/migrate/20190306055708_create_countries.rb
@@ -0,0 +1,10 @@
+class CreateCountries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :countries do |t|
+      t.string :country_name
+      t.integer :area
+
+      t.timestamps
+    end
+  end
+end
