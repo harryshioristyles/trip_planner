5ad973d6bba4052226c2eee6271bc5d7cc3e7b89
harryshioristyles <imshiori46@gmail.com>
2019-03-08 22:51:38 +0900

    Add controller and views for user_image Active_Storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-      params.require(:user).permit(:nickname, :introduce)
+      params.require(:user).permit(:nickname, :introduce, :user_image)
     end
 
 end
