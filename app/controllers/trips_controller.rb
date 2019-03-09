14cdd78fea4fe8ec3da6e5af70ccf397d82f6301
harryshioristyles <imshiori46@gmail.com>
2019-03-09 20:56:06 +0900

    Add world_areas to routes, controller, and views

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,4 +1,14 @@
 class TripsController < ApplicationController
+
+  def world; end
+  def area_asia; end
+  def area_middle_east; end
+  def area_europe; end
+  def area_africa; end
+  def area_north_america; end
+  def area_south_america; end
+  def area_oceania; end
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
 
 
-  root :to => 'users#top'
-  get 'admins/top' => 'admins#top'
+  root :to => "users#top"
+  get "admins/top" => "admins#top"
 
   devise_for :admins, controllers: {
     sessions:      'admins/sessions',
@@ -17,7 +17,19 @@ Rails.application.routes.draw do
   }
 
   resources :users, only: [:show, :update, :edit]
+
+  # trips
   resources :trips, only: [:index, :create, :show, :edit, :update, :destroy]
+
+  #maps
+  get "/world" => "trips#world", as:"world"
+  get "/area/asia" => "trips#area_asia", as:"asia"
+  get "/area/middle_east" => "trips#area_middle_east", as:"middle_east"
+  get "/area/europe" => "trips#area_europe", as: "europe"
+  get "/area/africa" => "trips#area_africa", as: "africa"
+  get "/area/north_america" => "trips#area_north_america", as: "north_america"
+  get "/area/south_america" => "trips#area_south_america", as: "south_america"
+  get "/area/oceania" => "trips#area_oceania", as: "oceania"
 
   namespace :admin do
     resources :cities, only: [:index, :create, :edit, :update, :destroy]
