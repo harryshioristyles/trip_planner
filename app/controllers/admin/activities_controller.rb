aed3e73071a02b16aab971b268796c09c99d6b7b
harryshioristyles <imshiori46@gmail.com>
2019-03-09 16:31:39 +0900

    Add acticities#show, #edit, #update, #destroy

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -16,15 +16,36 @@ class Admin::ActivitiesController < ApplicationController
   end
 
   def show
+          @activity = Activity.find(params[:id])
   end
 
   def edit
+          @activity = Activity.find(params[:id])
   end
 
   def update
+          activity = Activity.find(params[:id])
+    if
+          activity.update(activity_params)
+          redirect_to admin_activity_path(activity)
+          flash[:notice] = "successfully updated"
+    else
+          redirect_to edit_admin_path(activity)
+          flash[:notice] = 'update error!!'
+    end
+
   end
 
   def destroy
+          activity = Activity.find(params[:id])
+    if
+          activity.destroy
+          redirect_to admin_activities_path
+          flash[:notice] = "successfully updated"
+    else
+          redirect_to admin_activity_path(activity)
+          flash[:notice] = 'destroy error!!'
+    end
   end
 
   private
