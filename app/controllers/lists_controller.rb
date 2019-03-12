2e86f0d8fc42a0f8328f2e40fd0800ce5f42a755
harryshioristyles <imshiori46@gmail.com>
2019-03-12 15:20:35 +0900

    Add lists#create

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -2,12 +2,17 @@ class ListsController < ApplicationController
   def index
   end
 
-  def create
-  end
-
   def new
         @trip = Trip.where(user_id: current_user.id).last
         @list = List.new
+  end
+
+  def create
+        list = List.new(list_params)
+        trip_id = Trip.where(user_id: current_user.id).last
+        list.trip_id = trip_id.id
+        list.save!
+        redirect_to new_list_path
   end
 
   def show
@@ -21,4 +26,11 @@ class ListsController < ApplicationController
 
   def destroy
   end
+
+private
+
+  def list_params
+        params.require(:list).permit(:list_title, :list_details, :day_index, :date, :begin, :end, :city_id, :activity_id)
+  end
+
 end
