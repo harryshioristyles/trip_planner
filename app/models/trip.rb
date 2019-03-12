bbd901ae606882396ad760709234263c278573ea
harryshioristyles <imshiori46@gmail.com>
2019-03-12 19:26:54 +0900

    Add search to trips#index

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,4 +5,12 @@ class Trip < ApplicationRecord
 
   enum checking_finish: {not_finish: 0, finished: 1}
 
+    def self.search(search)
+      if search
+        Trip.where(['trip_title LIKE ?', "%#{search}%"])
+      else
+        Trip.all
+      end
+    end
+
 end
