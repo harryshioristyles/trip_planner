f80d51c34ea9b569e184f7125b2502047daf7841
harryshioristyles <imshiori46@gmail.com>
2019-03-13 16:17:14 +0900

    Add order by :day_index and :begin to Trip_model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,7 @@
 class Trip < ApplicationRecord
 
 	belongs_to :user
-    has_many :lists, dependent: :destroy
+    has_many :lists, dependent: :destroy, -> {order(:day_index, :begin)}
 
   enum checking_finish: {not_finish: 0, finished: 1}
 
