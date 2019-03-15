8d0801fef15cca683b49d631f77acca218016597
harryshioristyles <imshiori46@gmail.com>
2019-03-15 20:48:24 +0900

    Add FavoriteTrip model

---
+++ app/models/favorite_trip.rb
@@ -0,0 +1,2 @@
+class FavoriteTrip < ApplicationRecord
+end
---
+++ test/models/favorite_trip_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FavoriteTripTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
