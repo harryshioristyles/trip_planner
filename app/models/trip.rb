20ff063ca9910b0076adaf93c12fda603dba2de5
harryshioristyles <imshiori46@gmail.com>
2019-03-15 23:25:37 +0900

    Add favorites to model, controller, view

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,6 +4,11 @@ class Trip < ApplicationRecord
     has_many :lists, dependent: :destroy, -> {order(:day_index, :begin)}
     has_many :favorite_trips, dependent: :destroy
 
+    def favorited_by?(user)
+      favorite_trips.where(user_id: user.id).exists?
+    end
+
+
   enum checking_finish: {not_finish: 0, finished: 1}
 
     def self.search(search)
