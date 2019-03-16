5bd45d1c69f9bfd28b03195973b32ca6e058866a
harryshioristyles <imshiori46@gmail.com>
2019-03-16 20:37:36 +0900

    Add destroy and create_tags to trip models

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -22,4 +22,22 @@ class Trip < ApplicationRecord
         Trip.all
       end
     end
+
+  def save_tags(tags)
+    current_tags = self.tags.pluck(:tag) unless self.tags.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    # Destroy old taggings:
+    old_tags.each do |old_tag|
+      self.tags.delete Tag.find_by(tag:old_tag)
+    end
+
+    # Create new taggings:
+    new_tags.each do |new_tag|
+      trip_tag = Tag.find_or_create_by(tag:new_tag)
+      self.tags << trip_tag
+    end
+  end
+
 end
