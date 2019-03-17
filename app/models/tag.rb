38f0fb8b37a5f5415c71c3e49c0792df6260a9d2
harryshioristyles <imshiori46@gmail.com>
2019-03-17 18:03:12 +0900

    Switch line association before has_many

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,8 @@ class Tag < ApplicationRecord
 
 	validates :tag, presence:true, length:{maximum:50}
 
-    has_many :trips, through: :trip_tags
     has_many :trip_tags, dependent: :destroy
+    has_many :trips, through: :trip_tags
+
 
 end
