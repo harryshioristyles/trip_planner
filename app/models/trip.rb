5df0636cec21feb2f6a4cfcba62e6b6f1057a556
harryshioristyles <imshiori46@gmail.com>
2019-03-18 14:29:52 +0900

    Add validation to Trip_model
      validates :trip_title,      presence: true, length: { in: 2..20 }
      validates :trip_details,    length: { maximum: 50 }
      validates :checking_finish, numericality: { only_integer: true }
      validates :user_id,         numericality: { only_integer: true }

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,23 +1,29 @@
 class Trip < ApplicationRecord
 
-	belongs_to :user
+  validates :trip_title,      presence: true, length: { in: 2..20 }
+  validates :trip_details,    length: { maximum: 50 }
+  validates :checking_finish, numericality: { only_integer: true }
+  validates :user_id,         numericality: { only_integer: true }
 
-    has_many :lists, ->{order(:day_index, :begin)}, dependent: :destroy
-    has_many :favorite_trips, dependent: :destroy
+  belongs_to :user
 
-    has_one_attached :trip_image
+  has_many :lists, ->{order(:day_index, :begin)}, dependent: :destroy
+  has_many :favorite_trips, dependent: :destroy
 
-    enum checking_finish: {not_finish: 0, finished: 1}
+  has_one_attached :trip_image
 
-    def favorited_by?(user)
-       favorite_trips.where(user_id: user.id).exists?
+  enum checking_finish: {not_finish: 0, finished: 1}
+
+  def favorited_by?(user)
+     favorite_trips.where(user_id: user.id).exists?
+  end
+
+  def self.search(search)
+    if search
+      Trip.where(['trip_title LIKE ?', "%#{search}%"])
+    else
+      Trip.all
     end
+  end
 
-    def self.search(search)
-      if search
-        Trip.where(['trip_title LIKE ?', "%#{search}%"])
-      else
-        Trip.all
-      end
-    end
 end
