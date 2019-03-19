36848fba06c5e8e7a19dc99b85defa4508ca9388
harryshioristyles <imshiori46@gmail.com>
2019-03-19 12:45:58 +0900

    Change Log_in_info_email to nickname
    Log_in  : nickname and password
    Sign_in : nickname, email and password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,:email])
+  end
+
 end
