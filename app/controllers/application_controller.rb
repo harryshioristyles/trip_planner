e325a67e6f74c12aed06d635353204469ed871a2
harryshioristyles <imshiori46@gmail.com>
2019-03-23 19:55:52 +0900

    Fix after_log_in_faild
    redirect_to before_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
 
+  before_action :store_user_location!, if: :storable_location?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -10,6 +11,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname,:email])
   end
 
+
   def after_sign_in_path_for(resources)
     top_path(current_user)
   end
@@ -18,4 +20,15 @@ class ApplicationController < ActionController::Base
     root_path
   end
 
+  def storable_location?
+    request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+  end
+
+  def store_user_location!
+    store_location_for(:user, request.fullpath)
+  end
+
+    # def after_sign_in_path_for(resource_or_scope)
+    #   stored_location_for(resource_or_scope) || super
+    # end
 end
