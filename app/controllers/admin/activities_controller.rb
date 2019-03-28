6c7792795bee440e9fb02ca18a45f06316a1401d
harryshioristyles <imshiori46@gmail.com>
2019-03-28 14:58:30 +0900

    Fix indents

--- app/controllers/admin/activities_controller.rb
+++ app/controllers/admin/activities_controller.rb
@@ -1,56 +1,58 @@
 class Admin::ActivitiesController < ApplicationController
   def index
-          @activity = Activity.new
-          @activities = Activity.all
+      @activity = Activity.new
+      @activities = Activity.all
   end
 
   def create
-              activity = Activity.new(activity_params)
-    if    activity.save
-              flash[:notice] = "created successfully!"
-              redirect_to admin_activities_path
+      activity = Activity.new(activity_params)
+    if
+      activity.save
+      flash[:notice] = "created successfully!"
+      redirect_to admin_activities_path
     else
-              flash[:notice] = "Error"
-              render action: :index
+      flash[:notice] = "Error"
+      render action: :index
     end
+
   end
 
   def show
-          @activity = Activity.find(params[:id])
+      @activity = Activity.find(params[:id])
   end
 
   def edit
-          @activity = Activity.find(params[:id])
+      @activity = Activity.find(params[:id])
   end
 
   def update
-          activity = Activity.find(params[:id])
+      activity = Activity.find(params[:id])
     if
-          activity.update(activity_params)
-          redirect_to admin_activity_path(activity)
-          flash[:notice] = "successfully updated"
+      activity.update(activity_params)
+      redirect_to admin_activity_path(activity)
+      flash[:notice] = "successfully updated"
     else
-          redirect_to edit_admin_path(activity)
-          flash[:notice] = 'update error!!'
+      redirect_to edit_admin_path(activity)
+      flash[:notice] = 'update error!!'
     end
 
   end
 
   def destroy
-          activity = Activity.find(params[:id])
+      activity = Activity.find(params[:id])
     if
-          activity.destroy
-          redirect_to admin_activities_path
-          flash[:notice] = "successfully updated"
+      activity.destroy
+      redirect_to admin_activities_path
+      flash[:notice] = "successfully updated"
     else
-          redirect_to admin_activity_path(activity)
-          flash[:notice] = 'destroy error!!'
+      redirect_to admin_activity_path(activity)
+      flash[:notice] = 'destroy error!!'
     end
   end
 
   private
 
-      def activity_params
-        params.require(:activity).permit(:activity_name, :description, :city_id, :activity_image)
-      end
+  def activity_params
+    params.require(:activity).permit(:activity_name, :description, :city_id, :activity_image)
+  end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,40 +1,39 @@
 class Admin::UsersController < ApplicationController
   def index
-	  	    @users = User.all
+	  	@users = User.all
   end
 
   def edit
-	  	    @user = User.find(params[:id])
+	  	@user = User.find(params[:id])
   end
 
   def update
-  	    user = User.find(params[:id])
-	  if
-		  	user.update(user_params)
-		  	redirect_to admin_users_path
-		  	flash[:notice] = "successfully updated"
-	  else
-	  	  redirect_to edit_admin_user_path(user.id)
-        flash[:notice] = 'update error!!'
+	    user = User.find(params[:id])
+    if
+	  	user.update(user_params)
+	  	redirect_to admin_users_path
+	  	flash[:notice] = "successfully updated"
+    else
+  	  redirect_to edit_admin_user_path(user.id)
+      flash[:notice] = 'update error!!'
     end
   end
 
   def destroy
-        user = User.find(params[:id])
+      user = User.find(params[:id])
     if
-        user.destroy
-        redirect_to admin_users_path
-        flash[:notice] = 'Book was successfully destroyed.'
+      user.destroy
+      redirect_to admin_users_path
+      flash[:notice] = 'Book was successfully destroyed.'
     else
-        redirect_to edit_admin_user_path(user.id)
-        flash[:notice] = 'destroy error!!'
+      redirect_to edit_admin_user_path(user.id)
+      flash[:notice] = 'destroy error!!'
     end
   end
 
-    private
+  private
 
-    def user_params
-      params.require(:user).permit(:nickname, :introduction, :user_image)
-    end
-
+  def user_params
+    params.require(:user).permit(:nickname, :introduction, :user_image)
+  end
 end
