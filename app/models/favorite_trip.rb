5c4e522134f2ef2eb62d14284f331c6bfcf31481
harryshioristyles <imshiori46@gmail.com>
2019-03-29 05:52:38 +0000

    Remove validation only_integer from model

--- app/models/favorite_trip.rb
+++ app/models/favorite_trip.rb
@@ -1,8 +1,5 @@
 class FavoriteTrip < ApplicationRecord
 
-	validates :user_id, numericality: { only_integer: true }
-	validates :trip_id, numericality: { only_integer: true }
-
 	belongs_to :user
 	belongs_to :trip
 
