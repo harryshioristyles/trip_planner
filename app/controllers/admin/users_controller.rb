d6be7c8253ce6ecd1e75167038c6e54749e45dd7
harryshioristyles <imshiori46@gmail.com>
2019-03-29 07:15:36 +0000

    Retouch Admin::users#index and #edit

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,40 +1,41 @@
 class Admin::UsersController < ApplicationController
   def index
-	  	    @users = User.all
+	  	@users = User.all
   end
 
   def edit
-	  	    @user = User.find(params[:id])
+	  	@user = User.find(params[:id])
+      @users = User.all
   end
 
   def update
-  	    user = User.find(params[:id])
+  	  user = User.find(params[:id])
 	  if
-		  	user.update(user_params)
-		  	redirect_to admin_users_path
-		  	flash[:notice] = "successfully updated"
+		  user.update(user_params)
+		  redirect_to admin_users_path
+		  flash[:notice] = "successfully updated"
 	  else
-	  	  redirect_to edit_admin_user_path(user.id)
-        flash[:notice] = 'update error!!'
+	  	redirect_to edit_admin_user_path(user.id)
+      flash[:notice] = 'update error!!'
     end
   end
 
   def destroy
-        user = User.find(params[:id])
+      user = User.find(params[:id])
     if
-        user.destroy
-        redirect_to admin_users_path
-        flash[:notice] = 'Book was successfully destroyed.'
+      user.destroy
+      redirect_to admin_users_path
+      flash[:notice] = 'Book was successfully destroyed.'
     else
-        redirect_to edit_admin_user_path(user.id)
-        flash[:notice] = 'destroy error!!'
+      redirect_to edit_admin_user_path(user.id)
+      flash[:notice] = 'destroy error!!'
     end
   end
 
     private
 
-    def user_params
-      params.require(:user).permit(:nickname, :introduction, :user_image)
-    end
+  def user_params
+    params.require(:user).permit(:nickname, :introduction, :user_image)
+  end
 
 end
