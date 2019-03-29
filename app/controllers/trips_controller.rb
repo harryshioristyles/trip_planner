ad10d9b02d0d7d5df6d5cc4019c4e9164e756cea
harryshioristyles <imshiori46@gmail.com>
2019-03-29 13:16:27 +0000

    Fix page not to show Next

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -7,8 +7,13 @@ class TripsController < ApplicationController
 
       page_no = params[:id].to_i
       @trips = all_trips[page_no*10..page_no*10+9]
-      @next_page = page_no + 1
       @previous_page = page_no - 1
+    if
+      all_trips.last == @trips.last
+      @next_page = 0
+    else
+      @next_page = page_no + 1
+    end
   end
 
   def favorite_trips
@@ -17,8 +22,13 @@ class TripsController < ApplicationController
 
       page_no = params[:id].to_i
       @trips = all_trips[page_no*10..page_no*10+9]
-      @next_page = page_no + 1
       @previous_page = page_no - 1
+    if
+      all_trips.last == @trips.last
+      @next_page = 0
+    else
+      @next_page = page_no + 1
+    end
   end
 
   def new
