4beefa6ab943bffad1a39b84d0dcc4e35e707a09
harryshioristyles <imshiori46@gmail.com>
2019-03-29 05:53:25 +0000

    Remove validation only:integer from model
    Remove city_id and activity_id

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,8 +3,6 @@ class List < ApplicationRecord
  	validates :list_title,   presence: true, length: { in: 2..20 }
  	validates :list_details, length: { maximum: 200 }
 	validates :day_index,    numericality: { only_integer: true }
-	validates :city_id,      numericality: { only_integer: true }
-	validates :activity_id,  numericality: { only_integer: true }
 	validates :trip_id,      numericality: { only_integer: true }
 
 	belongs_to :city
