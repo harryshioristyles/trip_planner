907a2788435383dfdf0cf2cef9edc8d871fc079b
harryshioristyles <imshiori46@gmail.com>
2019-03-29 05:36:41 +0000

    Retouch cities#index and Remove validation
    Retouch cities#index_view and controller
    Add particial templete
    Remove validation country_id:integer, because model thinks integer must be there

--- app/controllers/admin/cities_controller.rb
+++ app/controllers/admin/cities_controller.rb
@@ -1,55 +1,55 @@
 class Admin::CitiesController < ApplicationController
   def index
-          @country = Country.new
-          @city = @country.cities.build
+      @country = Country.new
+      @city = @country.cities.build
 
-          @countries = Country.all.order(:area)
+      @countries = Country.all.order(:area)
   end
 
   def create
-          country = Country.new(country_params)
-    if country.save
-          flash[:notice] = "created successfully!"
-          redirect_to admin_cities_path
+      country = Country.new(country_params)
+    if
+      country.save!
+      flash[:notice] = "created successfully!"
     else
-          flash[:notice] = "Error"
-          render action: :index
+      flash[:notice] = "Error"
     end
+      redirect_to admin_cities_path
   end
 
   def edit
-          @country = Country.find(params[:id])
+      @country = Country.find(params[:id])
+      @countries = Country.all.order(:area)
   end
 
   def update
-          country = Country.find(params[:id])
+      country = Country.find(params[:id])
     if
-          country.update(country_params)
-          redirect_to admin_cities_path
-          flash[:notice] = "successfully updated"
+      country.update(country_params)
+      redirect_to admin_cities_path
+      flash[:notice] = "successfully updated"
     else
-          redirect_to edit_admin_city_path(country)
-          flash[:notice] = 'update error!!'
+      redirect_to edit_admin_city_path(country)
+      flash[:notice] = 'update error!!'
     end
   end
 
   def destroy
-          country = Country.find(params[:id])
-
+      country = Country.find(params[:id])
     if
-          country.destroy
-          redirect_to admin_cities_path
-          flash[:notice] = "successfully updated"
+      country.destroy
+      redirect_to admin_cities_path
+      flash[:notice] = "successfully updated"
     else
-          redirect_to edit_admin_city_path(country)
-          flash[:notice] = 'update error!!'
+      redirect_to edit_admin_city_path(country)
+      flash[:notice] = 'update error!!'
     end
   end
 
   private
 
-    def country_params
-        params.require(:country).permit(:country_name,:area, cities_attributes: [:id, :city_name, :country_id, :_destroy])
-    end
+  def country_params
+      params.require(:country).permit(:country_name,:area, cities_attributes: [:id, :city_name, :country_id, :_destroy])
+  end
 
 end
--- app/models/city.rb
+++ app/models/city.rb
@@ -1,7 +1,6 @@
 class City < ApplicationRecord
 
  	validates :city_name, presence: true, length: { maximum: 85 }
-	validates :country_id, numericality: { only_integer: true }
 
 	belongs_to :country
 
