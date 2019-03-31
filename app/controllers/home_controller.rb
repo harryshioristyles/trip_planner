50d8fadc5e73a69353a022229c9b919921fd8407
harryshioristyles <imshiori46@gmail.com>
2019-03-31 08:54:17 +0000

    Fix limit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
 	def top
 		@tags = Tag.find(TripTag.group(:tag_id).order('count(tag_id) desc').limit(20).pluck(:tag_id))
-	    @new_trips = Trip.where(checking_finish: 1).limit(21).order(updated_at: :desc)
+	    @new_trips = Trip.where(checking_finish: 1).limit(19).order(updated_at: :desc)
 	end
 
 end
